68c2da12bd381e444f2871a4be084ba180f7748b
Bron Davies <bron@brontech.com>
2025-07-18 13:17:34 -0600

    Update build script output

--- build.ps1
+++ build.ps1
@@ -4,10 +4,10 @@ $root = $PSScriptRoot
 $csproj = "$root\src\TrayToolbar\TrayToolbar.csproj"
 $version = ([xml](Get-Content $csproj)).Project.PropertyGroup.Version
 
-msbuild /t:restore $csproj
+msbuild -t:restore $csproj
 
-msbuild /t:Publish -p:RuntimeIdentifier=win-arm64 $csproj /p:PublishUrl=${root}\publish /p:Configuration=Release -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:SelfContained=false -p:PublishProtocol=FileSystem
+msbuild -t:Publish -p:RuntimeIdentifier=win-arm64 $csproj -p:PublishDir=${root}\publish -p:Configuration=Release -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:SelfContained=false -p:PublishProtocol=FileSystem
 Compress-Archive "$root\publish\*.exe" "$root\TrayToolbar-win-arm64-portable-$version.zip" -Force
 
-msbuild /t:Publish -p:RuntimeIdentifier=win-x64 $csproj /p:PublishUrl=${root}\publish /p:Configuration=Release -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:SelfContained=false -p:PublishProtocol=FileSystem
+msbuild -t:Publish -p:RuntimeIdentifier=win-x64 $csproj -p:PublishDir=${root}\publish -p:Configuration=Release -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:SelfContained=false -p:PublishProtocol=FileSystem
 Compress-Archive "$root\publish\*.exe" "$root\TrayToolbar-win-x64-portable-$version.zip" -Force
